9351540e79b0422b0794ccf448f2eed1b01cd436
masaya0519 <masayangs0519@gmail.com>
2022-04-06 05:28:22 +0000

    device

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :members
+
+  #管理者
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+  #会員
+  devise_for :member,skip: [:passwords], controllers: {
+  registrations: "member/registrations",
+  sessions: 'member/sessions'
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
