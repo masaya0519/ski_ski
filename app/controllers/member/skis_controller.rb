15301f2c80371fb6734d5e87f80beb3e070d0af6
masaya0519 <masayangs0519@gmail.com>
2022-04-13 07:00:32 +0000

    ジャンル検索機能

--- app/controllers/member/skis_controller.rb
+++ app/controllers/member/skis_controller.rb
@@ -2,5 +2,9 @@ class Member::SkisController < ApplicationController
   def show
     @ski = Ski.find(params[:id])
     @genres = Genre.all
+    if params[:genre_id] != nil
+      @skis = @skis.where(genre_id: params[:genre_id])
+    end
+    # @genre = Genre.find(params[:genre_id])
   end
 end
